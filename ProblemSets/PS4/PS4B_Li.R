daea61108f2ace881f80f82367d7bb193f463e69
Econ Account <ouecon008@schooner1.oscer.ou.edu>
2018-02-13 15:14:09 -0600

    HW submission

---
+++ ProblemSets/PS4/PS4B_Li.R
@@ -0,0 +1,12 @@
+#E2 
+df1<-iris
+df<- createDataFrame(df1)
+class(df1)
+class(df)
+head(select(df, df$Sepal_Length, df$Species))
+head(filter(df, df$Sepal_Length>5.5))
+head(select(filter(df, df$Sepal_Length>5.5), df$Sepal_Length, df$Species))
+
+df2<-head(summarize(groupBy(df, df$Species), mean=mean(df$Sepal_Length), count=n(df$Sepal_Length)))
+
+#head(arrange(df2, asc(df2$Species)))
